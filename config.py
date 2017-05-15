4b74af3aaa034786b78d607e49363cda8000c6fb
Petra Vidnerova <petra.vidnerova@gmail.com>
2017-05-15 10:43:33 +0200

    config for sensors and mnist

--- config.py
+++ config.py
@@ -1,17 +1,45 @@
 
-class Cfg:
+class CfgSensors:
 
-    input_shape = (8,)
-    noutputs = 1
+    batch_size = 100
+    epochs = 500
+    loss = 'mean_squared_error'
 
-    #batch_size = 100
-    #epochs = 500 
+    task_type = 'approximation'
+
+    pop_size = 30
+    ngen = 100
+
+    MAX_LAYERS = 5
+    MAX_LAYER_SIZE = 100
+    MIN_LAYER_SIZE = 5
+    DROPOUT = [ 0.0, 0.2, 0.3, 0.4 ] 
+    ACTIVATIONS = [ 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear' ] 
+
+
+class CfgMnist:
+
+#    input_shape = (8,)
+#    noutputs = 1
     
     batch_size = 128
     epochs = 20 
-
+    loss = 'categorical_crossentropy'
+    #loss = 'mean_squared_error'
+    
     task_type = "classification"
 
+    pop_size = 10
+    ngen = 10 
+
+    MAX_LAYERS = 3
+    MAX_LAYER_SIZE = 20
+    MIN_LAYER_SIZE = 10 
+    DROPOUT = [ 0.0, 0.2, 0.3, 0.4 ] 
+    ACTIVATIONS = [ 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear' ] 
+
     
-Config = Cfg()
+
+    
+Config = CfgMnist()
 
