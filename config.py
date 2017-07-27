f8c286ace283f84d7d8e36b3babcfcf1202c8843
Petra Vidnerova <petra.vidnerova@gmail.com>
2017-07-27 13:58:10 +0200

    new config for convolutional nets

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 class CfgSensors:
 
     batch_size = 100
-    epochs = 500
+    epochs = 10 #500
     loss = 'mean_squared_error'
 
     task_type = 'approximation'
 
     pop_size = 10
-    ngen = 10
+    ngen = 30
 
     MAX_LAYERS = 5
     MAX_LAYER_SIZE = 100
@@ -27,7 +27,7 @@ class CfgSensorsES:
 
     MU = 10
     LAMBDA = 30
-    ngen = 20
+    ngen = 100
 
     MAX_LAYERS = 5
     MAX_LAYER_SIZE = 100
@@ -45,21 +45,37 @@ class CfgSensorsES:
 class CfgMnist:
 
     batch_size = 128
-    epochs = 20 
-    #loss = 'categorical_crossentropy'
-    loss = 'mean_squared_error'
+    epochs = 10
+    loss = 'categorical_crossentropy'
+    #loss = 'mean_squared_error'
     
     task_type = "classification"
 
-    pop_size = 30
-    ngen = 100 
+    pop_size = 5
+    ngen = 5
 
     MAX_LAYERS = 5
-    MAX_LAYER_SIZE = 1000
+    MAX_LAYER_SIZE = 300
     MIN_LAYER_SIZE = 10 
     DROPOUT = [ 0.0, 0.2, 0.3, 0.4 ] 
     ACTIVATIONS = [ 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear' ] 
 
+    # for convolutional networks
+    MIN_FILTERS = 10
+    MAX_FILTERS = 50 
+    MIN_KERNEL_SIZE = 2
+    MAX_KERNEL_SIZE = 5
+    MIN_POOL_SIZE = 2
+    MAX_POOL_SIZE = 3
+    MAX_CONV_LAYERS = 3
+    MAX_DENSE_LAYERS = 3
+    
+    #DENSE_LAYER = 0.5
+    CONV_LAYER = 0.7 
+    MAX_POOL_LAYER = 0.3 
+
+    
+    
 class CfgMnistES:
 
     batch_size = 128
@@ -84,8 +100,8 @@ class CfgMnistES:
     DROPOUT = [ 0.0, 0.2, 0.3, 0.4 ] 
     ACTIVATIONS = [ 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear' ] 
 
-    
-#Config = CfgMnistES()
-Config = CfgSensorsES()  
+Config = CfgSensors()    
+#Config = CfgMnist()
+#Config = CfgSensorsES()  
 #Config = CfgMnist()
 
