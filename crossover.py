2bb53a61a68801b421fab406f0f2c3c299c9c01a
Petra Vidnerova <petra.vidnerova@gmail.com>
2017-07-27 14:45:44 +0200

    conv. crossover moved to crossover.py

--- crossover.py
+++ crossover.py
@@ -1,14 +1,30 @@
 import random 
 
+def cxListOnePoint(list1, list2):
+    assert len(list1) > 0 and len(list2) > 0
+    
+    cxpoint1 = random.randint(0, len(list1) - 1)
+    cxpoint2 = random.randint(0, len(list2) - 1)
+    list1[cxpoint1:], list2[cxpoint2:] = list2[cxpoint2:], list1[cxpoint1:]
+    
+    assert len(list1) > 0 and len(list2) > 0 
+    
+
 class Crossover:
 
     def cxOnePoint(self, ind1, ind2):
         #do one-point crossover on list of layers 
-        cxpoint1 = random.randint(0, len(ind1.layers) - 1)
-        cxpoint2 = random.randint(0, len(ind2.layers) - 1)
-        ind1.layers[cxpoint1:], ind2.layers[cxpoint2:] = ind2.layers[cxpoint2:], ind1.layers[cxpoint1:]
+        cxListOnePoint(ind1.layers, ind2.layers) 
+        return ind1, ind2 
 
-        assert len(ind1.layers) > 0
-        assert len(ind2.layers) > 0 
+    
+class CrossoverConv:
+
+    def cxOnePoint(self, ind1, ind2):
+        #do one-point crossover on list of layers
+        #convolutional layers
+        cxListOnePoint(ind1.conv_layers, ind2.conv_layers)
+        #dense layers 
+        cxListOnePoint(ind1.dense_layers, ind2.dense_layers)
         
         return ind1, ind2 
