565d682749587bd212e68cbd39d104b250627db8
Petra Vidnerová <petra.vidnerova@gmail.com>
2018-04-23 14:31:55 +0200

    cifar10 added

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from keras.datasets import mnist
+from keras.datasets import mnist, cifar10
 from keras.utils import np_utils
 
 
@@ -52,7 +52,31 @@ def load_data(filename):
 
         return X_test, Y_test 
     
+    # load cifar data set
+    if filename == "data/cifar10.train":
+        (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+        
+        X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+        X_train = X_train.astype('float32')
+        X_train /= 255
 
+        Y_train = np_utils.to_categorical(y_train, 10)
+
+        return X_train, Y_train 
+
+    if filename == "data/cifar10.test":
+        (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+        
+        X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+        X_test = X_test.astype('float32')
+        X_test /= 255
+
+        Y_test = np_utils.to_categorical(y_test, 10)
+
+        return X_test, Y_test 
+       
+
+    
     # load sensor data from csv file 
     data = pd.read_csv(filename, sep=';')
     X  = data[data.columns[:-1]].as_matrix()
