edb870f080304e8ebd5d724d410dd388bd6046e5
Petra Vidnerová <petra.vidnerova@gmail.com>
2018-04-26 11:31:12 +0200

    fashion_mnist added

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from keras.datasets import mnist, cifar10
+from keras.datasets import mnist, cifar10, fashion_mnist
 from keras.utils import np_utils
 
 
@@ -75,10 +75,28 @@ def load_data(filename):
        
     # load fashion-mnist set
     if filename == "data/fashion_mnist.train":
-        pass
+        (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+        X_train = X_train.reshape(-1, 28, 28, 1)
+        X_train = X_train.astype('float32')
+        X_train /= 255
+
+        Y_train = np_utils.to_categorical(y_train, 10)
+
+        return X_train, Y_train
+    
 
     if filename == "data/fashion_mnist.test":
-        pass
+        (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+        X_test = X_test.reshape(-1, 28, 28, 1)
+        X_test = X_test.astype('float32')
+        X_test /= 255
+
+        Y_test = np_utils.to_categorical(y_test, 10)
+
+        return X_test, Y_test 
+
     
     
     # load sensor data from csv file 
